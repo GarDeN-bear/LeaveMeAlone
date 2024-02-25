b5af22ff367db45c0535f3a968b0df46ee6e4f6d
Denis Garpinich <d.garpinich@wisetech.pro>
2024-02-25 13:39:25 +0300

    first commit

---
+++ Source/LeaveMeAlone/Public/Enemy/LMAEnemyStrongCharacter.h
@@ -0,0 +1,17 @@
+// LeaveMeAlone Game by Netologiya. All RightsReserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy/LMAEnemyCharacter.h"
+#include "LMAEnemyStrongCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LEAVEMEALONE_API ALMAEnemyStrongCharacter : public ALMAEnemyCharacter
+{
+	GENERATED_BODY()
+	
+};
